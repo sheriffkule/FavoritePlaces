779a02f38ec7ae67038f8b6f1bf0d7d90dab679f
sheriffkule <zeljko225@yahoo.com>
2022-09-09 20:38:57 +0200

    Favourite places start code

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+export default HelloWorldApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
