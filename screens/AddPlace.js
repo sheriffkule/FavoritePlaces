2979409503fcea3554e8061e531bd79116be16ae
sheriffkule <zeljko225@yahoo.com>
2022-09-24 10:50:44 +0200

    outputting a list of places

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import PlaceForm from '../components/Places/PlaceForm';
 
-function AddPlace() {
-  return <PlaceForm />;
+function AddPlace({navigation}) {
+  function createPlaceHandler(place) {
+    navigation.navigate('AllPlaces', {
+      place: place,
+    });
+  }
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
 }
 
 export default AddPlace;
