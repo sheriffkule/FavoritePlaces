c6c14975290260702fe80dc42ba1045d997e15f9
sheriffkule <zeljko225@yahoo.com>
2022-09-25 21:35:37 +0200

    getting started with sqlite

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import Map from './screens/Map';
 import IconButton from './components/UI/IconButton';
 import {Colors} from './constants/colors';
+import {init} from './util/database';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return <SplashScreen />;
+  }
+
   return (
     <>
       <StatusBar barStyle={'light-content'} />
